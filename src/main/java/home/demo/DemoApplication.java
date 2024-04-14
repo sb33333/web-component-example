f251ec1f26fff145904da8627d94cb8454fbcb70
sb33333 <springbean3@outlook.kr>
2024-04-14 15:56:17 +0900

    04.14.

--- src/main/java/home/demo/DemoApplication.java
+++ src/main/java/home/demo/DemoApplication.java
@@ -1,8 +1,19 @@
 package home.demo;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +21,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/{templateName}")
+    public ModelAndView template (@PathVariable String templateName) {
+        return new ModelAndView(templateName);
+    }
+    @PostMapping("/echo")
+    @ResponseBody
+    public Map<String, String[]> echo(HttpServletRequest request) {
+        return request.getParameterMap();
+    }
 }
